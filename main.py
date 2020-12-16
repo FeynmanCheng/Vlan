b929dcbaef0b4374f87890b7ee0082da55a0f567
Cheng <feynmancheng@github.com>
2020-12-16 12:40:43 +0800

    init

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from flask import Flask
-import telnet
+from flask.globals import request
+from telnet import Telnet
 app = Flask(__name__)
+
+TN = Telnet()
 
 @app.route('/')
 def hello_world():
-    telnet.get_connection()
     return 'Hello, World!'
 
-def get_telnet():
-    pass 
+@app.route('/login/<string:ip>')
+def login(ip):
+    user = request.args.get('user')
+    passwd = request.args.get('passwd')
+    
+    tn = TN.login(ip,user,passwd)
+    
+    return tn.read_until(b'hadoop@Slave2:~$',1).decode('ascii')
+
+@app.route('/command/<string:cmd>')
+def send_command(cmd):
+    cmd += '\n'
+    tn = TN.get_tn()
+    tn.write(cmd.encode('utf-8'))
+    return tn.read_until(b'hadoop@Slave2:~$',1).decode('ascii')
+
--- telnet.py
+++ telnet.py
@@ -1,33 +1,52 @@
 import telnetlib
 
-HOST = "172.19.241.171"
-user = "hadoop"
-password = " "
 
-def login():
-  tn = telnetlib.Telnet(HOST)
 
-  tn.read_until(b"login: ")
-  tn.write(user.encode('ascii') + b"\n")
-  if password:
-      tn.read_until(b"Password: ")
-      tn.write(password.encode('ascii') + b"\n")
-      
-  return tn
+class Telnet:
+  test_user = "hadoop"
+  test_passwd = "0"
 
-def get_connection():
-  tn = login()
-  print(tn.read_until(b'hadoop@Slave2:~$',1).decode('ascii'),end=' ')
-
+  HOST = "172.19.241.171"
   
-  while True:
-    cmd = input() + '\n'
-    tn.write(cmd.encode('utf-8'))
+  R = ''
+  S1 = ''
+  S2 = ''
+  S3 = ''
 
+  tn = None
+
+  def __init__(self) -> None:
+      super().__init__()
+      # self.tn = self.login(self.HOST, self.test_user, self.test_passwd)
+
+  def get_tn(self):
+    return self.tn
+
+  def login(self,ip, user, passwd):
+    self.tn = telnetlib.Telnet(self.HOST)
+
+    self.tn.read_until(b"login: ")
+    self.tn.write(user.encode('ascii') + b"\n")
+    if passwd:
+        self.tn.read_until(b"Password: ")
+        self.tn.write(passwd.encode('ascii') + b"\n")
+        
+    return self.tn
+
+  def get_connection(self):
+    tn = self.login(1,"hadoop",'0')
     print(tn.read_until(b'hadoop@Slave2:~$',1).decode('ascii'),end=' ')
 
-    if cmd == 'exit':
-      break
+    
+    while True:
+      cmd = input() + '\n'
+      tn.write(cmd.encode('utf-8'))
+
+      print(tn.read_until(b'hadoop@Slave2:~$',1).decode('ascii'),end=' ')
+
+      if cmd == 'exit':
+        break
 
 if __name__ == "__main__":
-    get_connection()
+    TN = Telnet()
+    TN.get_connection()
